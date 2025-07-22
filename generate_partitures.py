1237b17cc7ae235ec02e102f23cbf1639002f3e0
GerardAsbert <114249151+GerardAsbert@users.noreply.github.com>
2025-07-22 11:13:43 +0200

    Cleaner generate_partitures.py

--- generate_partitures.py
+++ generate_partitures.py
@@ -1,39 +1,30 @@
-from re import M
+import argparse
 import cv2
-import smashcima as sc
-import importlib.util
-import sys
 import os
+from smashcima.orchestration import TweakedHandwrittenModel, BaseHandwrittenModel
 
-'''file_path = "/home/gasbert/Desktop/Mashcima2/Smashcima/smashcima/orchestration/TweakedHandwrittenModel.py"
-module_name = "TweakedHandwrittenModel"
+def main(model_type: str, input_path: str, output_path: str):
+    if model_type == "base":
+        model = BaseHandwrittenModel()
+    elif model_type == "tweaked":
+        model = TweakedHandwrittenModel()
+    else:
+        raise ValueError("Model type must be either 'base' or 'tweaked'.")
 
-# Load the module
-spec = importlib.util.spec_from_file_location(module_name, file_path)
-module = importlib.util.module_from_spec(spec)
-sys.modules[module_name] = module
-spec.loader.exec_module(module)'''
-
-from smashcima.orchestration import TweakedHandwrittenModel
-from smashcima.orchestration import BaseHandwrittenModel
-
-model = BaseHandwrittenModel()
-
-'''for file in os.listdir("/data/gasbert/datasets/datasets/generation_data/Mashcima/Writer01"):
-    filename_mxml = file[:-3] + "musicxml"
-    if filename_mxml == "FMT_C47_0001b.01.musicxml":
-        scene = model("/data/gasbert/datasets/datasets/generation_data/TranscriptionsSMashcima/" + filename_mxml)
+    for file in os.listdir(input_path):
+        scene = model(os.path.join(input_path, file))
         for i, page in enumerate(scene.pages):
             bitmap = scene.render(page)
-            #cv2.imwrite(f"page_{i}_quarterrests.png", bitmap)
-            cv2.imwrite("dolores_partitures_wholepages/" + file, bitmap)'''
+            cv2.imwrite(os.path.join(output_path, file[:-8] + "png"), bitmap)
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Render MusicXML using Base or Tweaked Smashcima model.")
+    parser.add_argument("model_type", choices=["base", "tweaked"], help="Which model to use: 'base' or 'tweaked'")
+    parser.add_argument("input_path", type=str, help="Path to the MusicXML file")
+    parser.add_argument("output_path", type=str, help="Directory to save rendered PNG files")
 
+    args = parser.parse_args()
 
-scene = model("/data2fast/users/gasbert/datasets/generation_data/Transcriptions/UAB_LICEU_222570.059.08.musicxml")
-
-for i, page in enumerate(scene.pages):
-    bitmap = scene.render(page)
-    #cv2.imwrite(f"page_{i}_quarterrests.png", bitmap)
-    cv2.imwrite(f"prova_uab_liceu.png", bitmap)
+    os.makedirs(args.output_path, exist_ok=True)
+    main(args.model_type, args.input_path, args.output_path)
